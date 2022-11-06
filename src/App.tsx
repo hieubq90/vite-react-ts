3aa61f04a81228b68707c1dc7075f0cf78944252
hieubq90 <hieubq90@gmail.com>
2022-11-06 09:32:28 +0700

    update optimized icons

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ const App: FC = () => (
   <div className="gradient-card ">
     <h1 className="text-3xl font-bold text-bubble-gum">Hello world!</h1>
     <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-      AAAAAA
+      <MyIconsKnife />
+      <MyIconsCup />
     </button>
   </div>
 )
